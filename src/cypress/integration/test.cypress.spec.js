6eff7a1e87035c09589f597f96234d9a17504cc9
Danna <wdanna_@outlook.com>
2020-04-17 10:45:12 +0800

    Implement  login page API via cypress

--- src/cypress/integration/test.cypress.spec.js
+++ src/cypress/integration/test.cypress.spec.js
@@ -1,6 +1,6 @@
 describe("Login", () => {
   before(() => {
-    cy.visit("http://localhost:8080");
+    cy.visit("http://localhost:8081");
   });
 
   it("header content is correct", () => {
@@ -11,7 +11,9 @@ describe("Login", () => {
     cy.get(".login__email").type("danna");
     cy.get(".login__password").type("123456");
     cy.get(".login-sign-in__button").click();
+  });
 
+  it("login API works", () => {
     cy.server();
     cy.route({
       method: "POST",
@@ -20,6 +22,16 @@ describe("Login", () => {
         status: 200,
         redirect: "/homepage"
       }
+    }).as("loginCheck");
+
+    cy.get(".login__email").type("danna");
+    cy.get(".login__password").type("123456");
+    cy.get(".login-sign-in__button").click();
+
+    cy.wait("@loginCheck").then(xhr => {
+      console.log(xhr.response.body);
+      expect(xhr.response.body.status).to.equal(200);
+      expect(xhr.response.body.redirect).to.equal("/homepage");
     });
   });
 });
